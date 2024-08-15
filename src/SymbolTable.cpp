286cb0ecafc904c692f5027a44358091be7232e7
VIL20776 <vil20776@uvg.edu.gt>
2024-08-14 23:08:29 -0600

    Symbol Table initial implementation

--- src/SymbolTable.cpp
+++ src/SymbolTable.cpp
@@ -1,7 +1,11 @@
 #include "SymbolTable.h"
 
+#include <utility>
+#include <set>
+
 SymbolTable::SymbolTable():
-    table()
+    scopes(0), current_scope(0),
+    scope_map(), table()
 {
 }
 
@@ -9,25 +13,45 @@ SymbolTable::~SymbolTable()
 {
 }
 
-bool SymbolTable::bind(SymbolData symbol)
-{
-    if (table.contains(symbol.name))
-        return false;
-
-    table[symbol.name] = symbol;
-    return true;
+void SymbolTable::insert(SymbolData &symbol) {
+    auto key = std::make_pair(symbol.name, current_scope);
+    table.emplace(key, symbol);
 }
 
-bool SymbolTable::lookup(std::string name)
-{
-    return table.contains(name);
+SymbolData SymbolTable::find(std::string &symbol_name, unsigned int &scope) {
+    // Try finding in current scope
+    auto key = std::make_pair(symbol_name, scope);
+    if (table.contains(key))
+        return table.at(key);
+    
+    auto [start, end] = scope_map.equal_range(current_scope);
+    SymbolData symbol;
+    for (auto s = start; s != end; s++)
+    {
+        symbol = find(symbol_name, s->second);
+        if (symbol.name == symbol_name)
+            return symbol;
+    }    
+    
+    return {};
 }
 
-bool SymbolTable::update(std::string name, SymbolData symbol)
-{
-    if (!table.contains(name))
-        return false;
+SymbolData SymbolTable::find(std::string &symbol_name) {
+    return find(symbol_name, current_scope);
+}
 
-    table[name] = symbol;
-    return true;
+void SymbolTable::update(std::string &symbol_name, SymbolData symbol) {
+    table.insert_or_assign(std::make_pair(symbol_name, current_scope), symbol);
+}
+
+int SymbolTable::enter() {
+    scope_map.emplace(++scopes, current_scope);
+    current_scope = scopes;
+    return current_scope;
+}
+
+int SymbolTable::exit() {
+    auto [start, end] = scope_map.equal_range(current_scope);
+    current_scope = start->second;
+    return current_scope;
 }
--- src/SymbolTable.h
+++ src/SymbolTable.h
@@ -1,35 +1,92 @@
 #pragma once
 
+#include <map>
 #include <string>
+#include <vector>
 #include <unordered_map>
 
 enum SymbolType: int {
     VARIABLE,
     FUNCTION,
+    PARAMETER,
     CLASS,
-    OBJECT
+    OBJECT,
+    CLOSURE
 };
 
+enum SymbolDataType: int {
+    UNDEFINED = 0,
+    NUMBER = -1,
+    STRING = -2,
+    BOOLEAN = -3,
+    NIL = -4,
+    ANY = -5 // Placeholder para otros tipos
+};
+
+/*
+name - Identificacion
+parent - Exclusivo de clases, indica de que clase hereda 
+namespace - Si pertenece a una funcion o clase, el nombre se indica aqui
+type - Tipo de simbolo
+data_type - Tipo de dato. 
+    Si es función o cerradura, es nil o el tipo de la variable a retornar.
+    Si es clase, valor que lo identifica.
+    Si es objeto, valor de la clase.
+    Valores negativos para identificar datos primitivos.
+    Valores positivos para clases definidas.
+    El valor 0 indica que no está definido.
+value - Valor contenido, exclusivo de variables.
+    Vacio para funciones, clases, objetos y cerraduras.
+scope - Ambito, 0 es global
+size - Tamaño del símbolo
+offset - ubicacion en memoria
+
+*/
 struct SymbolData {
-    std::string name; 
+    std::string name;
+    std::string parent;
     SymbolType type;
-    std::string data_type;
+    int data_type; 
     std::string value;
+    int scope;
     int size;
-    int method_table_index;
-    bool global;
+    int offset;
 };
 
 class SymbolTable
 {
 private:
-    std::unordered_map<std::string, SymbolData> table;
+
+    struct pair_hash {
+        template <class T1, class T2>
+        int operator() (const std::pair<T1, T2>& p) const {
+            auto hash1 = std::hash<T1>{}(p.first);
+            auto hash2 = std::hash<T2>{}(p.second);
+            return hash1 ^ hash2;
+        }
+    };
+
+    unsigned int scopes;
+    unsigned int current_scope;
+    std::multimap<unsigned int, unsigned> scope_map; // to -> from
+    std::unordered_map<std::pair<std::string, int>, SymbolData, pair_hash> table;
 public:
-    SymbolTable(/* args */);
+    SymbolTable();
     ~SymbolTable();
 
-    bool bind(SymbolData symbol);    
-    bool lookup(std::string name);
-    bool update(std::string name, SymbolData symbol);
+    // Current Scope operations
 
+    void insert(SymbolData &symbol);
+    SymbolData find(std::string &symbol_name);
+    void update(std::string &symbol_name, SymbolData symbol);
+
+    // Selected Scope operations
+
+    SymbolData find(std::string &symbol_name, unsigned int &scope);
+
+    /*Crea un nuevo ambito y lo selecciona como el ambito actual*/
+    int enter();
+
+    /*Cierra el ambito actual y regresa al ambito anterior*/
+    int exit();
 };
